e2be3addc08cf5cfe1a49b9744038cca01eb674f
K <39096245+KishtheBeast@users.noreply.github.com>
2026-03-18 19:04:27 -0400

    Initial content

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_version" {
+  description = "Version of the VPC module"
+  type        = string
+  default     = "5.0.0"
+}
+
+variable "rds_version" {
+  description = "Version of the RDS module"
+  type        = string
+  default     = "6.1.1"
+}
